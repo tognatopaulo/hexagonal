f64cdebf892a4cfdff564397631a51c84766a16a
tognatopaulo <paulo.tognato@hotmail.com>
2023-11-08 20:07:26 -0300

    21. Criando as configurações do Kafka

---
+++ src/main/java/com/tognati/hexagonal/adapter/in/consumer/message/CustomerMessage.java
@@ -0,0 +1,16 @@
+package com.tognati.hexagonal.adapter.in.consumer.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerMessage {
+    private String id;
+    private String name;
+    private String zipCode;
+    private String cpf;
+    private Boolean isValidCpf;
+}
